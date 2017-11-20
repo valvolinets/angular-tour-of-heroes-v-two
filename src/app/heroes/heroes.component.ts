a44054f75597f7b4c03023a2eab4a26ee4794130
Valerijs Volinecs <Val@Valerijss-MacBook-Air.local>
2017-11-20 17:08:34 +0200

    step 3. generated HeroesComponent using cli. added two-way data binding with the ngModel directive.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+hero: Hero = {
+  id: 1,
+  name: "Batman"
+};
+}
